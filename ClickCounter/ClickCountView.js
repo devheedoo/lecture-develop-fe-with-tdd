98b96ff078182d3f689116285cb418ff1f59d049
Heedo Kim <devheedoo@gmail.com>
2019-10-15 20:10:46 +0900

    Lecture: ClickCountView Module - Spec 1-2

--- ClickCounter/ClickCountView.js
+++ ClickCounter/ClickCountView.js
@@ -1,6 +1,7 @@
 var App = App || {};
 
 App.ClickCountView = (clickCounter, updateEl) => {
+  if (!clickCounter || !updateEl) throw Error('no clickCounter or updateEl');
   return {
     updateView() {
       updateEl.innerHTML = clickCounter.getValue();
--- ClickCounter/ClickCountView.spec.js
+++ ClickCounter/ClickCountView.spec.js
@@ -5,6 +5,19 @@ describe('App.ClickCountView', ()=> {
     updateEl = document.createElement('span');
     view = App.ClickCounterView(clickCounter, updateEl);
   })
+
+  it('clickCounter를 주입하지 않으면 에러를 던진다', () => {
+    const clickCounter = null;
+    const actual = () => App.ClickCcounterView(clickCounter, updateEl);
+    expect(actual).toThrowError();
+  });
+
+  it('updateEl을 주입하지 않으면 에러를 던진다', () => {
+    const updateEl = null;
+    const actual = () => App.ClickCcounterView(clickCounter, updateEl);
+    expect(actual).toThrowError();
+  });
+
   describe('updateView()', ()=> {
     it('ClickCounter의 getValue() 값을 출력한다', ()=> {
       const counterValue = clickCounter.getValue();
