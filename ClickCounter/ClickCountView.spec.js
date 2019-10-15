ac809209b56c0922f7ad15c3831ba2c042ecbe0b
Heedo Kim <devheedoo@gmail.com>
2019-10-15 20:01:32 +0900

    ClickCountView spec 1-1

--- ClickCounter/ClickCountView.spec.js
+++ ClickCounter/ClickCountView.spec.js
@@ -1,7 +1,15 @@
 describe('App.ClickCountView', ()=> {
+  let updateEl, clickCounter, view;
+  beforeEach(() => {
+    clickCounter = App.ClickCounter();
+    updateEl = document.createElement('span');
+    view = App.ClickCounterView(clickCounter, updateEl);
+  })
   describe('updateView()', ()=> {
     it('ClickCounter의 getValue() 값을 출력한다', ()=> {
-      // todo 
+      const counterValue = clickCounter.getValue();
+      view.updateView();
+      expect(updateEl.innerHTML).toBe(counterValue.toString());
     })
   })
 })
