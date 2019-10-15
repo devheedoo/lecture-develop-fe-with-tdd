e47f76baa425fe8c765ea5debb5423bbde704351
Heedo Kim <devheedoo@gmail.com>
2019-10-15 19:34:50 +0900

    Lecture: ClickCounter Module Spec 2

--- ClickCounter/ClickCounter.spec.js
+++ ClickCounter/ClickCounter.spec.js
@@ -1,7 +1,9 @@
+// describe는 무한 중첩 가능
 describe('App.ClickCounter', ()=> {
   describe('getValue()', ()=> {
     it('초기값이 0인 카운터 값을 반환한다', ()=> {
-      // todo 
+      const counter = App.ClickCounter();
+      expect(counter.getValue()).toBe(0);
     })
   })
 })
